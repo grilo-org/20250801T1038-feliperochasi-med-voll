e9afe9842e6bfa377bea1fbc10d358834fea345a
Felipe Rocha <darkkfoxx.z@gmail.com>
2025-05-28 14:53:09 -0300

    Tratando erro 404

---
+++ src/main/java/br/com/feliperochasi/med/voll/api/infra/ErrorsHandler.java
@@ -0,0 +1,15 @@
+package br.com.feliperochasi.med.voll.api.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorsHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity errorHandler404() {
+        return ResponseEntity.notFound().build();
+    }
+}
