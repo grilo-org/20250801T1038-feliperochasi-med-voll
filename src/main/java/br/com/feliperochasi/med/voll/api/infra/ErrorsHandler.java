6ff9cc916b15a9e62aca9864d7f6565aea83e307
Felipe Rocha <darkkfoxx.z@gmail.com>
2025-05-28 15:08:19 -0300

    Tratando erro 400

--- src/main/java/br/com/feliperochasi/med/voll/api/infra/ErrorsHandler.java
+++ src/main/java/br/com/feliperochasi/med/voll/api/infra/ErrorsHandler.java
@@ -2,6 +2,8 @@ package br.com.feliperochasi.med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,5 +13,17 @@ public class ErrorsHandler {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity errorHandler404() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity errorHandler400(MethodArgumentNotValidException ex) {
+        var errors = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(errors.stream().map(ErrorsDataValidation::new).toList());
+    }
+
+    private record ErrorsDataValidation(String campo, String mensagem){
+        public ErrorsDataValidation(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
     }
 }
