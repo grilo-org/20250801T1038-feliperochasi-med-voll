80b51033cf45e0d31afbeadbcea3338ade3f49c3
Felipe Rocha <darkkfoxx.z@gmail.com>
2025-05-29 14:36:23 -0300

    Configuracoes de seguranca

---
+++ src/main/java/br/com/feliperochasi/med/voll/api/infra/security/SecurityConfigurations.java
@@ -0,0 +1,22 @@
+package br.com.feliperochasi.med.voll.api.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity //sinalizar que vamos configurar os metodos de seguranca
+public class SecurityConfigurations {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+
+}
