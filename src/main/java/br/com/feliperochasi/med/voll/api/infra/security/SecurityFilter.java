d904b5dcbf65942b0f233f12a45f5c2df6ce123a
Felipe Rocha <darkkfoxx.z@gmail.com>
2025-05-30 11:51:39 -0300

    Criando o filter de seguranca

---
+++ src/main/java/br/com/feliperochasi/med/voll/api/infra/security/SecurityFilter.java
@@ -0,0 +1,19 @@
+package br.com.feliperochasi.med.voll.api.infra.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component //identifica a classe para o spring como se fosse uma classe generica
+public class SecurityFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        filterChain.doFilter(request, response);
+    }
+}
