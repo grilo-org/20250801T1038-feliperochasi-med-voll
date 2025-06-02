68b9822e1e2989f1a99af701f448dc070d04ac6e
Felipe Rocha <darkkfoxx.z@gmail.com>
2025-06-02 15:13:46 -0300

    Validacoes de integridade

--- src/main/java/br/com/feliperochasi/med/voll/api/domain/consultation/ScheduleConsultation.java
+++ src/main/java/br/com/feliperochasi/med/voll/api/domain/consultation/ScheduleConsultation.java
@@ -1,5 +1,7 @@
 package br.com.feliperochasi.med.voll.api.domain.consultation;
 
+import br.com.feliperochasi.med.voll.api.domain.ValidationIdException;
+import br.com.feliperochasi.med.voll.api.domain.medic.Medic;
 import br.com.feliperochasi.med.voll.api.domain.medic.MedicRepository;
 import br.com.feliperochasi.med.voll.api.domain.patient.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,13 +20,23 @@ public class ScheduleConsultation {
     private PatientRepository patientRepository;
 
     public void scheduler(ScheduleConsultationData data) {
+        if (!patientRepository.existsById(data.idPaciente())) {
+            throw new ValidationIdException("ID do paciente informado nao existe!");
+        }
 
+        if (data.idMedico() != null && !medicRepository.existsById(data.idMedico())) {
+            throw  new ValidationIdException("ID do medico informado nao existe!");
+        }
 
-
-
-        var medic = medicRepository.getReferenceById(data.idMedico());
+        var medic = chooseMedic(data);
         var patient = patientRepository.getReferenceById(data.idPaciente());
         var consultation = new Consultation(null, medic, patient, data.data());
         consultationRepository.save(consultation);
+    }
+
+    private Medic chooseMedic(ScheduleConsultationData data) {
+
+
+        return null;
     }
 }
