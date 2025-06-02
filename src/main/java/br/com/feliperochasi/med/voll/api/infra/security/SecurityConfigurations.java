d28c9f67daa4e9bd2660f8c865507548e04e34dd
Felipe Rocha <darkkfoxx.z@gmail.com>
2025-06-02 11:01:22 -0300

    Mudanca nos filtros chamados antes da requisicao

--- src/main/java/br/com/feliperochasi/med/voll/api/infra/security/SecurityConfigurations.java
+++ src/main/java/br/com/feliperochasi/med/voll/api/infra/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package br.com.feliperochasi.med.voll.api.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,10 +11,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity //sinalizar que vamos configurar os metodos de seguranca
 public class SecurityConfigurations {
+
+    @Autowired
+    private SecurityFilter securityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -22,7 +27,7 @@ public class SecurityConfigurations {
                 .authorizeHttpRequests(req -> {
                     req.requestMatchers("/login").permitAll();
                     req.anyRequest().authenticated();
-                })
+                }).addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
