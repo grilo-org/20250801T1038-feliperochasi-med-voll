1ba3bbf8cdf964eb0d2870215798c4d6129add1e
Felipe Rocha <darkkfoxx.z@gmail.com>
2025-06-04 10:11:04 -0300

    DTO para recebimento da requisicao de cancelamento de consulta

---
+++ src/main/java/br/com/feliperochasi/med/voll/api/domain/consultation/CancellationConsultationData.java
@@ -0,0 +1,11 @@
+package br.com.feliperochasi.med.voll.api.domain.consultation;
+
+import jakarta.validation.constraints.NotNull;
+
+public record CancellationConsultationData(
+        @NotNull
+        Long idConsulta,
+        @NotNull
+        String motivo
+) {
+}
