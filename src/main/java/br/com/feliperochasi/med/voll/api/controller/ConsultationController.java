6416cc9ec25588a756b0a711c1f4aa52e724fefb
Felipe Rocha <darkkfoxx.z@gmail.com>
2025-06-04 11:26:02 -0300

    Documentacao da api usando springdoc

--- src/main/java/br/com/feliperochasi/med/voll/api/controller/ConsultationController.java
+++ src/main/java/br/com/feliperochasi/med/voll/api/controller/ConsultationController.java
@@ -3,6 +3,7 @@ package br.com.feliperochasi.med.voll.api.controller;
 import br.com.feliperochasi.med.voll.api.domain.consultation.CancellationConsultationData;
 import br.com.feliperochasi.med.voll.api.domain.consultation.ScheduleConsultation;
 import br.com.feliperochasi.med.voll.api.domain.consultation.ScheduleConsultationData;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/consultas")
+@SecurityRequirement(name = "bearer-key")
 public class ConsultationController {
 
     @Autowired
--- src/main/java/br/com/feliperochasi/med/voll/api/controller/MedicController.java
+++ src/main/java/br/com/feliperochasi/med/voll/api/controller/MedicController.java
@@ -1,6 +1,7 @@
 package br.com.feliperochasi.med.voll.api.controller;
 
 import br.com.feliperochasi.med.voll.api.domain.medic.*;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -13,6 +14,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/medicos")
+@SecurityRequirement(name = "bearer-key")
 public class MedicController {
 
     @Autowired
