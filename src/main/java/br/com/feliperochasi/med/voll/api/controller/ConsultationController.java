e1b2d0d1cd9316554aba9d3cc4812820acf2964a
Felipe Rocha <darkkfoxx.z@gmail.com>
2025-06-04 10:10:34 -0300

    Recebe chamada para cancelamento

--- src/main/java/br/com/feliperochasi/med/voll/api/controller/ConsultationController.java
+++ src/main/java/br/com/feliperochasi/med/voll/api/controller/ConsultationController.java
@@ -1,16 +1,13 @@
 package br.com.feliperochasi.med.voll.api.controller;
 
-import br.com.feliperochasi.med.voll.api.domain.consultation.DetailsConsultationData;
+import br.com.feliperochasi.med.voll.api.domain.consultation.CancellationConsultationData;
 import br.com.feliperochasi.med.voll.api.domain.consultation.ScheduleConsultation;
 import br.com.feliperochasi.med.voll.api.domain.consultation.ScheduleConsultationData;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/consultas")
@@ -24,5 +21,12 @@ public class ConsultationController {
     public ResponseEntity schedule(@RequestBody @Valid ScheduleConsultationData data) {
         var dto = service.scheduler(data);
         return ResponseEntity.ok(dto);
+    }
+
+    @DeleteMapping
+    @Transactional
+    public ResponseEntity cancel(@RequestBody @Valid CancellationConsultationData data) {
+        service.cancel(data);
+        return ResponseEntity.noContent().build();
     }
 }
