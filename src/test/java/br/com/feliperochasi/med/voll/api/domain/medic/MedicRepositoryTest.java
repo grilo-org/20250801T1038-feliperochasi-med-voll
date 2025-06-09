4e9f560b9a3bf38c5fb57d8beafbbf857fb4361e
Felipe Rocha <darkkfoxx.z@gmail.com>
2025-06-09 08:43:28 -0300

    Configurando o banco de testes para usar mysql ao inves de banco in memoria

---
+++ src/test/java/br/com/feliperochasi/med/voll/api/domain/medic/MedicRepositoryTest.java
@@ -0,0 +1,18 @@
+package br.com.feliperochasi.med.voll.api.domain.medic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+class MedicRepositoryTest {
+
+    @Test
+    void chooseMedicBySpecialised() {
+    }
+}
